a0e9f58a680457bb1b698048529d694500813143
SkyYap <32660903+SkyYap@users.noreply.github.com>
2024-11-10 03:48:19 +0800

    add gitignore and remove unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider, createConfig, http } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 import {
   mainnet,
   polygon,
@@ -9,7 +9,6 @@ import {
   arbitrum,
   base,
 } from 'wagmi/chains';
-import { injected } from 'wagmi/connectors';
 import App from './App';
 import './index.css';
 import '@rainbow-me/rainbowkit/styles.css'; 
